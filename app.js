970d535be3c3a67306f0965297445efc3a824e6b
Zahro Nurrobby Ashari <zahronurrobbya@gmail.com>
2021-06-03 10:13:58 +0700

    setTimeout(),setInterval,function es6,var es6 ,if else es6

---
+++ app.js
@@ -0,0 +1,20 @@
+const amount = 12;
+
+amount < 10 ? console.log("small number") : console.log("large number");
+
+console.log("hello world");
+
+console.log(__dirname);
+
+setTimeout(() => {
+  console.log("tempek");
+}, 60);
+
+const sayHi = (name) => {
+  console.log(`tempek ${name}`);
+};
+
+const tempek = "tempek";
+
+sayHi("stev");
+sayHi(tempek);
